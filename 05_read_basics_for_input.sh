36582c69c91037bec1492120dfcef474594ff4de
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 15:50:37 +0530

    Update 05_read_basics_for_input.sh

--- 05_read_basics_for_input.sh
+++ 05_read_basics_for_input.sh
@@ -19,6 +19,34 @@ echo $fruit1
 echo $fruit2
 echo $fruit3
 
-# -p PROMPT: Display a prompt before reading input.
-# -t TIMEOUT: Set a time limit for input. If the user does not provide input within TIMEOUT seconds, read will return a non-zero status.
-# -s: Silent mode. Do not echo input to the terminal (useful for passwords).
+###############################################################################################################
+#!/bin/bash
+# -p refers to print/prompt input statement, \t \n \v will be eliminated with this option
+read -p "Enter your name: " myname
+
+# -r To get special characters in the input such as \t \n \v
+read -rp "Enter your date of Birth: " myDOB
+
+# -s To make input's invisible to the user. This is used for password inputs
+# It is recommended to update echo after using -s option as it removes new line creation by default
+read -srp "Enter your password: " mypasswd
+echo
+
+# When you did not mention any variable name in read command, by defaul the input values are stored in $REPLY variable
+read -rp "Enter any value: "
+
+# You can get two inputs in single line with read command.
+read -rp "Enter you First name and Last name: " fname lname
+
+# You can use IFS field separator to split the inputs based on the Separator you mentioning
+IFS=":" read -rp "Enter you FirstName:LastName: " fNAME lNAME
+
+
+# Printing the outputs
+echo "My name is ${myname}, Date of Birth is ${myDOB} and password is ${mypasswd}"
+echo "My value is ${REPLY}"
+echo "My First name is: ${fname}"
+echo "My Last name is: ${lname}"
+echo "My 2nd First name: ${fNAME}"
+echo "My 2nd Last name: ${lNAME}"
+###################################################################################################################
