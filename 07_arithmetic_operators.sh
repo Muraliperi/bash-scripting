751c65f00698e056660b8572433c5b1967c0ab6c
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 13:58:32 +0530

    Update 07_arithmetic_operators.sh

--- 07_arithmetic_operators.sh
+++ 07_arithmetic_operators.sh
@@ -13,3 +13,12 @@ echo "c value is: $c"
 c value is: 300
 [root@ansible-control-node bash_automation]#
 ----------------------------------------------------------------
+root@ansible-control-node bash_automation]# echo $((23 + 78))
+101
+[root@ansible-control-node bash_automation]# echo $((23 - 78))
+-55
+[root@ansible-control-node bash_automation]# echo $((23 * 78))
+1794
+[root@ansible-control-node bash_automation]# echo $((23 / 78))
+0
+[root@ansible-control-node bash_automation]#
