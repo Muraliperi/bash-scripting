e018207b073b5a60f24244b41e83d084cc7088c6
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 14:31:12 +0530

    Update 06_command_line_arguments.sh

--- 06_command_line_arguments.sh
+++ 06_command_line_arguments.sh
@@ -3,3 +3,15 @@ a=$1
 b=$2
 results=`expr "$a + $b"| bc `
 echo "Addition of $a and $b is: $results"
+---------------------------------------------------------------------------
+#!/bin/bash
+echo "date"
+echo "The name of my script is: ${0}"
+echo "The value of First argument: ${1}"
+echo "The value of Second argument: ${2}"
+echo "The value of Third argument: ${3}"
+echo "Total number of arguments is: ${#}"
+echo "Total number of arguments : ${*}"
+IFS=','
+echo "Total number of arguments before IFS Internal Field Separator set to Comma character : ${*}"
+---------------------------------------------------------------------------
