ba49353f94343c6b302c457f9415870bc2e09141
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 14:15:30 +0530

    Update 13_if_else_condition.sh

--- 13_if_else_condition.sh
+++ 13_if_else_condition.sh
@@ -7,3 +7,19 @@ else
 fi
 
 # $1 is the command line argument. User needs to provide any number when executing this script. Ex: ./if_else_condition.sh 34
+
+#!/bin/bash
+if [ -f /tmp/testfile.txt ]
+then
+   echo "File exist"
+else
+   echo "File is missing"
+fi
+
+# [ -f file ] --> To validate file or path is a file or not
+# [ -d file ] --> To validate file or path is a directory or not
+# [ -e file ] --> To validate file exist or not
+# [ -s file ] --> To validate file size is greater than zero or not
+# [ -r file ] --> To validate file is having read permissions for the current user
+# [ -w file ] --> To validate file is having write permissions for the current user 
+# [ -x file ] --> To validate file is having execution permissions for the current user
