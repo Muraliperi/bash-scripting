e1f9257c3d160fabc5c607dec8a95fd06733ef2d
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 22:27:25 +0530

    Update 61_project_monitor_process_or_service.sh

--- 61_project_monitor_process_or_service.sh
+++ 61_project_monitor_process_or_service.sh
@@ -22,3 +22,36 @@ if [[ ${serviceCount} -eq 0 ]] ; then
 else
    ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service IS RUNNING" | ${TEE} -a ${LOGFILE}
    ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service IS RUNNING"
+--------------------------------------------------------------------------------------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat monitor_process_or_service2.sh
+#!/bin/bash
+declare -a services
+services=(httpd rsyslog sshd)
+HOSTNAME=/usr/bin/hostname
+MAILX=/usr/bin/mailx
+PS=/usr/bin/ps
+WC=/usr/bin/wc
+GREP=/bin/grep
+ECHO=/usr/bin/echo
+BASENAME=/usr/bin/basename
+DATE=/usr/bin/date
+DATETIME=$(date '+%F %T')
+TEE=/usr/bin/tee
+LOGFILE=/var/tmp/BashLogDir/$(${BASENAME} $0)_$(${DATE} '+%F').log2
+mailTo=muruganbcamba@gmail.com
+
+for serviceName in ${services[@]}
+do
+  serviceCount=$(${PS} auxf| ${GREP} ${serviceName} | ${GREP} -v grep | ${GREP} -v ${0} | ${WC} -l)
+  ${ECHO} "${DATETIME} : $(${HOSTNAME}) Process count value for ${serviceName} is : ${serviceCount}" | ${TEE} -a ${LOGFILE}
+
+  if [[ ${serviceCount} -eq 0 ]] ; then
+     ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service is NOT RUNNING" | ${TEE} -a ${LOGFILE}
+     ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service is NOT RUNNING" | ${MAILX} -s "Alert service ${serviceName} status" ${mailTo}
+  else
+     ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service IS RUNNING" | ${TEE} -a ${LOGFILE}
+     ${ECHO} "${DATETIME} : $(${HOSTNAME}) ${serviceName} service IS RUNNING"
+  fi
+done
+[root@ansible-control-node bash_automation]#
+--------------------------------------------------------------------------------------------------------------------------------------------------
