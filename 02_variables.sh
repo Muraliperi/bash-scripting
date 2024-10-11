0a452a2b35a210db8ca4ae5378700db2e590733d
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 15:52:41 +0530

    Update 02_variables.sh

--- 02_variables.sh
+++ 02_variables.sh
@@ -21,3 +21,18 @@ The value of z is 45
 The value of f is 89
 Fri Oct 11 00:47:59 EDT 2024
 [root@ansible-control-node bash_automation]#
+----------------------------------------------------------------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat common_vars
+#!/bin/bash
+ansible=$(ansible --version|awk 'NR==1 {print $3}'|cut -d ']' -f1)
+httpd=$(httpd -v|awk 'NR==1 {print $3}')
+[root@ansible-control-node bash_automation]# cat source_to_read_variables_from_different_scriptfile.sh
+#!/bin/bash
+source /root/bash_automation/common_vars
+echo "${httpd}"
+echo "${ansible}"
+[root@ansible-control-node bash_automation]# sh source_to_read_variables_from_different_scriptfile.sh
+Apache/2.4.37
+2.16.3
+[root@ansible-control-node bash_automation]#
+-------------------------------------------------------------------------------------------------------------------------
