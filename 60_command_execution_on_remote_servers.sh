b6bc75a2d587095f11dafc84613b7b4af94203e5
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 22:24:56 +0530

    Update 60_command_execution_on_remote_servers.sh

--- 60_command_execution_on_remote_servers.sh
+++ 60_command_execution_on_remote_servers.sh
@@ -11,3 +11,19 @@ do
         echo "Failed to execute the command ${reqCmd} on ${serverName}"
     fi
 done < <(cat remoteServers.txt | awk 'NR != 1 ')
+----------------------------------------------------------------------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat sample_Expect_to_autodetect_password.sh
+#!/bin/bash
+# AD-Hoc command to run any script for the server on which manual password input is needed
+userName=root
+serverName=rockylinux9
+passWord=abcd@1234
+reqCmd=${1}
+expect -c "
+          spawn ssh -o StrictHostKeyChecking=No ${userName}@${serverName} ${reqCmd}
+          expect {
+               "*password:*" {send $passWord\r ; interact }
+          }
+          exit
+          "
+[root@ansible-control-node bash_automation]#
