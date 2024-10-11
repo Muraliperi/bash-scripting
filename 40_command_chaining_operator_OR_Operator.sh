e897dc83848cebab3610a29826d672645f0b3d1a
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 20:58:18 +0530

    Update 40_command_chaining_operator_OR_Operator.sh

--- 40_command_chaining_operator_OR_Operator.sh
+++ 40_command_chaining_operator_OR_Operator.sh
@@ -7,3 +7,13 @@ which nginx 1>/dev/null 2>/dev/null  --> We are nullifying success output with 1
 # If first command failed then OR || operator will be executed thus "Nginx is not installed on this host" will be printed.
 which nginx 1>/dev/null 2>/dev/null && echo "Nginx is installed on this Host" || echo "Nginx is not installed on this host"
 command -v nginx 1>/dev/null 2>/dev/null && echo "Nginx is installed on this Host" || echo "Nginx is not installed on this host"
+-----------------------------------------------------------------------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat install_package_if_not_Found_or_not_exist.sh
+#!/bin/bash
+# Check if package exist, if not install it
+cmd=${1}
+command -v ${cmd} 1>/dev/null 2>/dev/null && { echo "${cmd} is installed"; echo "Path of ${cmd} is $(command -v ${cmd})"; } || { echo "Installing package of ${cmd}"; yum install ${cmd} -y; }
+[root@ansible-control-node bash_automation]# sh install_package_if_not_Found_or_not_exist.sh httpd
+httpd is installed
+Path of httpd is /usr/sbin/httpd
+[root@ansible-control-node bash_automation]#
