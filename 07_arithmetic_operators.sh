28dcb415e8ba69dffc03393fc1c450d64e7b22f9
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 13:44:23 +0530

    Update 07_arithmetic_operators.sh

--- 07_arithmetic_operators.sh
+++ 07_arithmetic_operators.sh
@@ -3,3 +3,13 @@ x=$1
 y=$2
 results=$((x+y))
 echo "The addition of $x and $y is: $results"
+----------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat scr.sh
+#!/bin/bash
+b=300
+c=`expr $a + $b`
+echo "c value is: $c"
+[root@ansible-control-node bash_automation]# sh scr.sh
+c value is: 300
+[root@ansible-control-node bash_automation]#
+----------------------------------------------------------------
