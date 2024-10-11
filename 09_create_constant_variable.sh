1fd19e2a8b656688155cfbf850ae63a07c336279
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 14:03:47 +0530

    Update 09_create_constant_variable.sh

--- 09_create_constant_variable.sh
+++ 09_create_constant_variable.sh
@@ -19,3 +19,26 @@ Murali
 [root@ansible-control-node bash_automation]# echo $abc
 10
 [root@ansible-control-node bash_automation]#
+-----------------------------------------------------------------------------------
+[root@ansible-control-node bash_automation]# cat make_constant_variable1.sh
+#!/bin/bash
+# using declare -i to keep the variable with particular data type. Here we are passing integer value, so we can not change it's value to any other data/variable type such as string
+declare -i ab=100
+declare -i bc=345
+echo "ab value is $ab"
+echo "bc value is $bc"
+cd=$(( ab+bc ))
+echo "ab + bc value is $cd"
+ab="Hello"
+echo "ab value after changing to Hello string $ab"
+echo "bc value is $bc"
+cd=$(( ab+bc ))
+echo "ab + bc after trying with updated ab value is $cd"
+[root@ansible-control-node bash_automation]# sh make_constant_variable1.sh
+ab value is 100
+bc value is 345
+ab + bc value is 445
+ab value after changing to Hello string 0
+bc value is 345
+ab + bc after trying with updated ab value is 345
+[root@ansible-control-node bash_automation]#
