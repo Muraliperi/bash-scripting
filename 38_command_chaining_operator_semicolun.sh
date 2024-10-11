fce83165627204f738af9965ec34c24b89308beb
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 19:53:24 +0530

    Create 38_command_chaining_operator_semicolun.sh

---
+++ 38_command_chaining_operator_semicolun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Commands will get executed on sequence basis
+# All commands executed independently. In case any of the command getting fails, it won't affect further commands execution
+date;uptime;uname -a;hostname
