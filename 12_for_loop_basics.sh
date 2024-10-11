a0ecd3f2597402223475c7223fa16e2ed157254d
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-11 14:42:05 +0530

    Update 12_for_loop_basics.sh

--- 12_for_loop_basics.sh
+++ 12_for_loop_basics.sh
@@ -66,3 +66,21 @@ echo "< $file >"
 [ -f $file ] && ls -lrt $file || echo "$file missing"
 echo "-----------"
 done
+--------------------------------------------------------
+# Generate chess board using Nested for loop
+#!/bin/bash
+for (( i=1; i<=8; i++ ))
+  do
+  for (( j=1; j<=8; j++ ))
+    do
+    total=$(( $i + $j ))
+    tmp=$(( $total % 2 ))
+    if [ $tmp -eq 0 ]; then
+    echo -e -n "\e[47m  \e[0m"
+    else
+    echo -e -n "\e[40m  \e[0m"
+    fi
+done
+echo ""
+done
+----------------------------------------------------------
